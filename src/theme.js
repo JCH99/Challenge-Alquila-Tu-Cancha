ed81e57ae734ecd826744168cab8b268f7610b2b
Joaquin Charovsky <joaquincharovsky@gmail.com>
2022-03-30 13:24:34 -0300

    custom colors theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,16 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      light: "#00c853",
+      main: "#57a051",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: "#f7c95c",
+      main: "#f56d20",
+    },
   },
 });
 
